3529014d870ea177253f031a21707ae83283037c
Marnix <33731014+TemplarMarnix@users.noreply.github.com>
2020-03-04 11:37:09 +0100

    making the index

--- common/links.php
+++ common/links.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link href="css/style.css" rel="stylesheet">
-<link href="css/extension.min.css" rel="stylesheet">
 <script src="https://kit.fontawesome.com/37e2a8fe9a.js" crossorigin="anonymous"></script>
 <link rel="shortcut icon" type="image/x-icon" href="media/favicon.png">
 <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -17,12 +17,23 @@
         <div class="row header">
             <?php require('header.php'); ?>
         </div>
-        <div class="row categories">
-            <div class="col-lg-12 col-md-12">
+        <div class="row content-top">
+            <div class="col-lg-2 col-md-2"><!-- leftside --></div>
+            <div class="col-lg-1 col-md-1">categorieën</div>
+            <?php
+                $string = $category;
+                $categoryName = preg_replace("/'/", "", $string);
+            ?>
+            <div class="col-lg-5 col-md-5"><?php echo $categoryName; ?></div>
+            <div class="col-lg-2 col-md-2">winkelwagen</div>
+            <div class="col-lg-2 col-md-2"><!-- rightside --></div>
+        </div>
+        <div class="row content-middle">
+            <div class="col-lg-2 col-md-2"></div>
+            <div class="col-lg-1 col-md-1">
                 <form method="post" action="">
                     <ul>
-                        <li><h2>Categorieën:</h2></li>
-                        <li><button type="submit" name="vipboxen">Vip Boxen</button></li>
+                        <li><button class="active-category" type="submit" name="vipboxen">Vip Boxen</button></li>
                         <li><button type="submit" name="appetizers">Appetizers</button></li>
                         <li><button type="submit" name="nigiri">Nigiri</button></li>
                         <li><button type="submit" name="hosomaki">Hosomaki</button></li>
@@ -37,32 +48,38 @@
                     </ul>
                 </form>
             </div>
+            <div class="col-lg-5 col-md-5 d-flex flex-wrap">
+                <?php
+                $sql = "SELECT * FROM product WHERE category=" . $category;
+                $result = $conn->query($sql);
+        
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        ?>
+                        <div class="col-lg-4 col-md-4">
+                            <div class="product">
+                                <img src="<?php echo $row['image']; ?>" alt="foto"><br>
+                                <div class="information">
+                                    <span><?php echo $row['name'] ?></span>
+                                    <hr>
+                                    <form method="post" action="index.php?action=add&id=<?php echo $row['id']; ?>">
+                                        <input type="number" name="quantity" min="1" value="1">
+                                        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>">
+                                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                                        <button type="submit" name="add_to_cart"><i class="fas fa-cart-plus"></i></button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    }
+                } else {
+                    echo "Sorry this product is sold out.";
+                }
+                ?>
+            </div>
         </div>
         <?php
-        $sql = "SELECT * FROM product WHERE category=" . $category;
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                ?>
-                <div class="product">
-                    <img src="<?php echo $row['image']; ?>" alt="foto"><br>
-                    <span><?php echo $row['name'] ?></span>
-                    <a href="product.php?id=<?php echo $row['id']; ?>">
-                        <div class="buttons">Zie meer ></div>
-                    </a>
-                    <form method="post" action="index.php?action=add&id=<?php echo $row['id']; ?>">
-                        <input type="number" name="quantity" value="1">
-                        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>">
-                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
-                        <button type="submit" name="add_to_cart">Add to Cart</button>
-                    </form>
-                </div>
-        <?php
-            }
-        } else {
-            echo "Sorry this product is sold out.";
-        }
         require('footer.php');
         $conn->close(); ?>
     </div>
