d0b94de8658c51d0490d3aae38f557a6c05acfd7
NielsPerdon <40392944+NielsPerdon@users.noreply.github.com>
2020-03-18 14:02:21 +0100

    alsjeblieft marnix

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    require('common/connect.php');
+    require('common/links.php');
+    require('common/category.php');
+    require('common/shoppingcart.php');
+
+    if(!isset($_SESSION['category'])) {
+        $_SESSION["category"] = "'vipboxen'";
+    } else {
+        $_SESSION["category"] = $category;
+    }
+
+    $curpage = 'index';
+    $home = '';
+    $order = '';
+    $about = '';
+    $contact = 'active-page';
+    ?>
